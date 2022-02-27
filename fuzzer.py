e6092dfb60857c8e27c27a3216beae004cb6b769
Akuli <akuviljanen17@gmail.com>
2022-02-28 01:05:05 +0200

    add dumb and effective fuzzer

---
+++ fuzzer.py
@@ -0,0 +1,20 @@
+import random
+import socket
+
+commands = [
+    b"\x08",
+    b"\x7f",
+    b"\x1b[A",
+    b"\x1b[B",
+    b"\x1b[C",
+    b"\x1b[D",
+    b"\r",
+    b"\n",
+    b"w",
+    b"a",
+    b"s",
+    b"d",
+]
+conn = socket.create_connection(("localhost", 12345))
+while True:
+    conn.send(random.choice(commands))
