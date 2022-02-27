31f90b111f6e42350a79bfec6004b410692c3c1a
Akuli <akuviljanen17@gmail.com>
2022-02-27 12:59:46 +0200

    copy/paste comment

--- catris.py
+++ catris.py
@@ -644,6 +644,7 @@ class RingGame(Game):
                     return False
         return True
 
+    # In ring mode, full lines are actually full squares.
     def find_full_lines(self) -> list[int]:
         return [
             r
