f1e34e4f10edd1e93fc80d6f7906d416436d0eab
Akuli <akuviljanen17@gmail.com>
2022-04-26 12:37:25 +0300

    Several fixes to disconnecting when send queue fills up (#131)

--- catris/server_and_client.py
+++ catris/server_and_client.py
@@ -67,6 +67,7 @@ class Client:
         self.server = server
         self._reader = reader
         self.writer = writer
+        self._current_receive_task: asyncio.Task[bytes] | None = None
         self._recv_stats: collections.deque[tuple[float, int]] = collections.deque()
 
         self.name: str | None = None
@@ -147,13 +148,23 @@ class Client:
         self._send_bytes(to_send)
 
     def _send_bytes(self, b: bytes) -> None:
+        if self.writer.transport.is_closing():
+            return
+
         self.writer.write(b)
 
         # Prevent filling the server's memory if client sends but never receives.
-        # I don't use .drain() because one client's slowness shouldn't slow others.
-        if self.writer.transport.get_write_buffer_size() > 64 * 1024:  # type: ignore
-            self.log("More than 64K of data in send buffer, disconnecting")
+        # Usually send buffer is empty (0 bytes) because operating system has buffering too.
+        # But it feels weird to rely on operating system's (undocumented?) buffer size.
+        #
+        # On 80x24 terminal with no colors, we send max 80*24 = 1920 bytes at a time.
+        # There's some extra space for colors and bigger terminals.
+        if self.writer.transport.get_write_buffer_size() > 4 * 1024:  # type: ignore
+            self.log("More than 4K of data in send buffer, disconnecting")
             self.writer.transport.close()
+            # Closing isn't enough to stop receiving immediately
+            if self._current_receive_task is not None:
+                self._current_receive_task.cancel()
 
     async def _receive_bytes(self) -> bytes | None:
         # Makes game playable while under very heavy cpu load.
@@ -163,8 +174,10 @@ class Client:
         if self.writer.transport.is_closing():
             return None
 
+        assert self._current_receive_task is None
+        self._current_receive_task = asyncio.create_task(self._reader.read(100))
         try:
-            result = await asyncio.wait_for(self._reader.read(100), timeout=3 * 60)
+            result = await asyncio.wait_for(self._current_receive_task, timeout=3 * 60)
         except asyncio.TimeoutError:
             self.log("Nothing received in 3min, disconnecting")
             self._send_bytes(
@@ -172,9 +185,14 @@ class Client:
                 + b"Closing connection because it has been idle for 3 minutes.\r\n"
             )
             return None
+        except asyncio.CancelledError:
+            # cancelled in _send_bytes()
+            return None
         except OSError as e:
             self.log(f"Receive error: {e}")
             return None
+        finally:
+            self._current_receive_task = None
 
         # Prevent 100% cpu usage if someone sends a lot of data
         now = time.monotonic()
