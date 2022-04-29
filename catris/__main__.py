54c3b416f743fa6830f8e0ceaa5742acb6701e2b
Akuli <akuviljanen17@gmail.com>
2022-04-29 21:15:32 +0300

    Change idle time from 3min to 10min (#143)

--- catris/__main__.py
+++ catris/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import asyncio
 import logging
+import socket
 
 from catris.server_and_client import Server
 
@@ -20,6 +21,12 @@ async def main() -> None:
     asyncio_server = await asyncio.start_server(
         catris_server.handle_connection, port=12345
     )
+
+    # Send TCP keepalive packets periodically as configured in /etc/sysctl.conf
+    # Prevents clients from disconnecting after about 5 minutes of inactivity.
+    for sock in asyncio_server.sockets:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+
     async with asyncio_server:
         logging.info("Listening on port 12345...")
         await asyncio_server.serve_forever()
