8ca40ef18a850fb4940bb0b695f09565cc3539bb
Akuli <akuviljanen17@gmail.com>
2022-05-23 21:30:01 +0300

    it sends stuff over socket, single client at a time

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::{TcpListener, TcpStream};
+use std::net::SocketAddr;
+use tokio::io::AsyncWriteExt;
+use tokio::time::sleep;
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() {
+    // Bind the listener to the address
+    let listener = TcpListener::bind("0.0.0.0:12345").await.unwrap();
+    println!("Listening!");
+
+    loop {
+        let (mut socket, sockaddr) = listener.accept().await.unwrap();
+        process(&mut socket, sockaddr).await;
+    }
+}
+
+async fn process(socket: &mut TcpStream, sockaddr: SocketAddr) {
+    println!("Processing!!! {}", sockaddr.ip());
+    socket.write(b"hello\r\n").await.unwrap();  // FIXME: don't panic
+    socket.write(b"lolwat\r\n").await.unwrap();  // FIXME: don't panic
+    sleep(Duration::from_secs(10)).await;
 }
