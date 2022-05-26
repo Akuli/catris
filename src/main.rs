ff210b0e885bb6d268671ad43e21d8e38f297f2f
Akuli <akuviljanen17@gmail.com>
2022-05-26 23:53:44 +0300

    parse received text to utf8 string

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::io::Write;
 use std::net::IpAddr;
+use std::sync::atomic::AtomicU64;
+use std::sync::atomic::Ordering;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::time::Duration;
+use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
-use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::tcp::OwnedReadHalf;
+use tokio::net::tcp::OwnedWriteHalf;
+use tokio::net::TcpListener;
+use tokio::net::TcpStream;
 use tokio::sync::watch;
 use tokio::time::sleep;
 
@@ -13,11 +18,93 @@ mod ansi;
 mod game_logic;
 mod render;
 
-use crate::render::RenderBuffer;
+static ID_COUNTER: AtomicU64 = AtomicU64::new(0);
 
-async fn handle_receiving(reader: OwnedReadHalf) {
+struct ConnectionLogger {
+    ip: IpAddr,
+    id: u64,
+}
+
+impl ConnectionLogger {
+    fn new(ip: IpAddr) -> ConnectionLogger {
+        let result = ConnectionLogger {
+            ip: ip,
+            // https://stackoverflow.com/a/32936288
+            id: ID_COUNTER.fetch_add(1, Ordering::SeqCst),
+        };
+        result.log(format!("New connection from {}", ip));
+        result
+    }
+
+    fn log(&self, message: String) {
+        println!("[client {}] {}", self.id, message);
+    }
+}
+
+impl Drop for ConnectionLogger {
+    fn drop(&mut self) {
+        self.log("Disconnected".to_string());
+    }
+}
+
+async fn handle_receiving(reader: &mut OwnedReadHalf, logger: &ConnectionLogger) {
+    let mut buffer = [0 as u8; 100];
+    let mut bytes_received = 0 as usize;
+    let mut text = "".to_string();
     loop {
-        sleep(Duration::from_millis(400)).await;
+        // Receive bytes. May be incomplete or invalid utf-8 characters, or
+        // incomplete ansi escape sequence.
+        match reader.read(&mut buffer[bytes_received..]).await {
+            Ok(n) => {
+                if n == 0 {
+                    // clean disconnect
+                    return;
+                }
+                bytes_received += n;
+            }
+            Err(e) => {
+                logger.log(format!("can't recv: {}", e));
+                return;
+            }
+        }
+
+        // Parse valid utf-8 parts. Can still contain incomplete ANSI escape sequences.
+        let parsed_start;
+        let parsed_end;
+        {
+            let mut to_parse = &buffer[0..bytes_received];
+            while to_parse.len() > 0 {
+                match std::str::from_utf8(to_parse) {
+                    Ok(s) => {
+                        text.push_str(&s);
+                        to_parse = &to_parse[to_parse.len()..];
+                    }
+                    Err(e) => {
+                        let (valid, after_valid) = to_parse.split_at(e.valid_up_to());
+                        text.push_str(std::str::from_utf8(valid).unwrap());
+                        if let Some(n) = e.error_len() {
+                            // not enough data to get any valid characters
+                            if n <= 0 {
+                                panic!("wat");
+                            }
+                            to_parse = &after_valid[n..];
+                        } else {
+                            // not enough data to continue parsing
+                            break;
+                        }
+                    }
+                }
+            }
+            parsed_start = to_parse.as_ptr() as usize - buffer.as_ptr() as usize;
+            parsed_end = to_parse.as_ptr() as usize - buffer.as_ptr() as usize;
+        }
+        for i in parsed_start..parsed_end {
+            buffer[i - parsed_start] = buffer[i];
+        }
+        bytes_received = parsed_end - parsed_start;
+
+        println!("got text: {}", text);
+        text = "".to_string();
     }
 }
 
@@ -25,38 +112,21 @@ async fn handle_sending(
     mut writer: OwnedWriteHalf,
     mut need_render_receiver: watch::Receiver<()>,
     game: Arc<Mutex<game_logic::Game>>,
+    logger: &ConnectionLogger,
 ) {
-    let mut last_rendered = RenderBuffer::new();
-    let mut currently_rendering = RenderBuffer::new();
+    let mut last_rendered = render::RenderBuffer::new();
+    let mut currently_rendering = render::RenderBuffer::new();
 
     loop {
         currently_rendering.clear();
         game.lock().unwrap().render_to_buf(&mut currently_rendering);
         let to_send = currently_rendering.get_updates_as_ansi_codes(&mut last_rendered);
-        /*if let Err(e) = writer.write_all(to_send.as_bytes()).await {
-            println!("send error: {}", e);
+        if let Err(e) = writer.write_all(to_send.as_bytes()).await {
+            logger.log(format!("can't send: {}", e));
             return;
-        }*/
-        writer.write_all(to_send.as_bytes()).await.unwrap();
+        }
         currently_rendering.copy_into(&mut last_rendered);
         need_render_receiver.changed().await.unwrap();
-    }
-}
-
-struct Connection {
-    ip: IpAddr,
-}
-
-impl Connection {
-    fn new(ip: IpAddr) -> Connection {
-        println!("Connection from {}", ip);
-        Connection { ip }
-    }
-}
-
-impl Drop for Connection {
-    fn drop(&mut self) {
-        println!("Disconnected: {}", self.ip);
     }
 }
 
@@ -66,11 +136,11 @@ async fn handle_connection(
     need_render_receiver: watch::Receiver<()>,
     game: Arc<Mutex<game_logic::Game>>,
 ) {
-    let _c = Connection::new(ip);
-    let (reader, writer) = socket.into_split();
+    let logger = ConnectionLogger::new(ip);
+    let (mut reader, writer) = socket.into_split();
     tokio::select! {
-        _ = handle_receiving(reader) => {},
-        _ = handle_sending(writer, need_render_receiver, game) => {},
+        _ = handle_receiving(&mut reader, &logger) => {},
+        _ = handle_sending(writer, need_render_receiver, game, &logger) => {},
     }
 }
 
