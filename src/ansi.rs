13d185cf39861f3a3e58b582afff482b06a3a41b
Akuli <akuviljanen17@gmail.com>
2022-05-28 21:37:47 +0300

    parse keyboard inputs better

--- src/ansi.rs
+++ src/ansi.rs
@@ -39,23 +39,31 @@ pub enum KeyPress {
     Right,
     Left,
     BackSpace,
-    Quit, // Ctrl+C or similar
+    Quit,
+    RefreshRequest,
     Character(char),
 }
+
+const NORMAL_BACKSPACE: &[u8] = b"\x7f";
+const WINDOWS_BACKSPACE: &[u8] = b"\x08";
+
+const CTRL_C: &[u8] = b"\x03";
+const CTRL_D: &[u8] = b"\x04";
+const CTRL_Q: &[u8] = b"\x11";
+const CTRL_R: &[u8] = b"\x12";
 
 // Returning None means need to receive more data.
 // The usize is how many bytes were consumed.
 pub fn parse_key_press(data: &[u8]) -> Option<(KeyPress, usize)> {
     match data {
-        b"" => None,
-        b"\x1b" => None,
-        b"\x1b[" => None,
+        b"" | b"\x1b" | b"\x1b[" => None,
         b"\x1b[A" => Some((KeyPress::Up, 3)),
         b"\x1b[B" => Some((KeyPress::Down, 3)),
         b"\x1b[C" => Some((KeyPress::Right, 3)),
         b"\x1b[D" => Some((KeyPress::Left, 3)),
-        b"\x7f" => Some((KeyPress::BackSpace, 1)), // linux/mac terminal
-        b"\x08" => Some((KeyPress::BackSpace, 1)), // windows cmd.exe
+        NORMAL_BACKSPACE | WINDOWS_BACKSPACE => Some((KeyPress::BackSpace, 1)),
+        CTRL_C | CTRL_D | CTRL_Q => Some((KeyPress::Quit, 1)),
+        CTRL_R => Some((KeyPress::RefreshRequest, 1)),
         // utf-8 chars are never >4 bytes long
         _ => match std::str::from_utf8(&data[0..min(data.len(), 4)]) {
             Ok(s) => {
