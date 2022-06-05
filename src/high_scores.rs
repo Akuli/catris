98e489bcc85b7a250fc54cd62d42cdb0486c4e63
Akuli <akuviljanen17@gmail.com>
2022-06-06 00:28:03 +0300

    it writes high score

--- src/high_scores.rs
+++ src/high_scores.rs
@@ -1,3 +1,4 @@
+use crate::game_logic::Mode;
 use std::io;
 use std::io::ErrorKind;
 use std::sync::Mutex;
@@ -11,20 +12,33 @@ use tokio::io::AsyncWriteExt;
 use tokio::io::BufReader;
 
 pub struct HighScore {
+    pub mode: Mode,
     pub score: usize,
     pub duration: Duration,
     pub players: Vec<String>,
 }
 
-async fn read_best_high_scores(file: &mut File) -> Result<(), io::Error> {
-    let mut lines = BufReader::new(file).lines();
-    Ok(())
+fn mode_to_string(mode: Mode) -> &'static str {
+    match mode {
+        Mode::Traditional => "traditional",
+        Mode::Bottle => "bottle",
+        Mode::Ring => "ring",
+    }
 }
+
+/*async fn read_best_high_scores(file: &mut File) -> Result<(), io::Error> {
+    let mut lines = BufReader::new(file).lines();
+
+    if lines.next_line().await? != Some("catris high scores file v{}\n".to_string()) {
+        return Err(io::Error::new(ErrorKind::Other, "unexpected first line in high scores file"));
+    }
+
+    Ok(())
+}*/
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 async fn ensure_file_exists(filename: &str) -> Result<(), io::Error> {
-    println!("Asd");
     match OpenOptions::new()
         .create_new(true)
         .write(true)
@@ -34,11 +48,26 @@ async fn ensure_file_exists(filename: &str) -> Result<(), io::Error> {
         Ok(mut file) => {
             file.write_all(format!("catris high scores file v{}\n", VERSION).as_bytes())
                 .await?;
+            println!("[high scores] Created {}", filename);
             Ok(())
         }
         Err(e) if e.kind() == ErrorKind::AlreadyExists => Ok(()),
         Err(e) => Err(e),
     }
+}
+
+async fn save_high_score(filename: &str, hs: &HighScore) -> Result<(), io::Error> {
+    let mut file = OpenOptions::new().append(true).open(filename).await?;
+    // TODO: change format, current format is pretty shit.
+    // Get rid of lobby id (here always "-"), add timestamps.
+    file.write_all(format!(
+        "{}\t-\t{}\t{}\t{}\n",
+        mode_to_string(hs.mode),
+        hs.score,
+        hs.duration.as_secs_f64(),
+        &hs.players.join("\t")
+    ).as_bytes()).await?;
+    Ok(())
 }
 
 // Prevent multiple games writing their high scores at once.
@@ -47,7 +76,9 @@ lazy_static! {
     static ref FILE_LOCK: Mutex<&'static str> = Mutex::new("catris_high_scores.txt");
 }
 
-pub async fn add_high_score(hs: &HighScore) {
+pub async fn add_high_score(hs: &HighScore) -> Result<(), io::Error> {
     let filename_handle = FILE_LOCK.lock().unwrap();
-    ensure_file_exists(*filename_handle).await;
+    ensure_file_exists(*filename_handle).await?;
+    save_high_score(*filename_handle, &hs).await?;
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use(lazy_static)]
 extern crate lazy_static;
 
+use crate::game_logic::Mode;
+use std::io;
 use std::time::Duration;
 
 mod ansi;
@@ -16,10 +18,13 @@ mod render;
 mod views;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), io::Error> {
     crate::high_scores::add_high_score(&crate::high_scores::HighScore {
+        mode: Mode::Ring,
         score: 123,
         duration: Duration::from_millis(678901),
         players: vec!["John".to_string(), "Bob".to_string(), "Mary".to_string()],
-    }).await;
+    })
+    .await?;
+    Ok(())
 }
