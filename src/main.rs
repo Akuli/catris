e73f28336dfbc5fc56ee3e3575a703b13ee40ffd
Akuli <akuviljanen17@gmail.com>
2022-06-07 17:31:42 +0300

    restore main.rs as it was

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 #[macro_use(lazy_static)]
 extern crate lazy_static;
 
-use crate::game_logic::Mode;
-use std::error::Error;
+use crate::render::RenderBuffer;
+use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::io;
+use std::net::IpAddr;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::time::Duration;
+use std::time::Instant;
+use tokio::io::AsyncWriteExt;
+use tokio::net::tcp::OwnedWriteHalf;
+use tokio::net::TcpListener;
+use tokio::net::TcpStream;
+use tokio::time::timeout;
+use weak_table::WeakValueHashMap;
 
 mod ansi;
 mod blocks;
@@ -18,14 +29,139 @@ mod player;
 mod render;
 mod views;
 
+async fn handle_receiving(
+    mut client: client::Client,
+    lobbies: lobby::Lobbies,
+    used_names: Arc<Mutex<HashSet<String>>>,
+) -> Result<(), io::Error> {
+    views::ask_name(&mut client, used_names).await?;
+    client
+        .logger()
+        .log(&format!("Name asking done: {}", client.get_name()));
+
+    let want_new_lobby = views::ask_if_new_lobby(&mut client).await?;
+    if want_new_lobby {
+        client.make_lobby(lobbies);
+    } else {
+        views::ask_lobby_id_and_join_lobby(&mut client, lobbies).await?;
+    }
+
+    let mut selected_index = 0 as usize;
+    loop {
+        let game_mode = views::choose_game_mode(&mut client, &mut selected_index).await?;
+        match game_mode {
+            Some(mode) => views::play_game(&mut client, mode).await?,
+            None => views::show_gameplay_tips(&mut client).await?,
+        }
+    }
+}
+
+async fn handle_sending(
+    writer: &mut OwnedWriteHalf,
+    render_data: Arc<Mutex<render::RenderData>>,
+) -> Result<(), io::Error> {
+    // pseudo optimization: double buffering to prevent copying between buffers
+    let mut buffers = [RenderBuffer::new(), RenderBuffer::new()];
+    let mut next_idx = 0;
+
+    loop {
+        let cursor_pos;
+        {
+            let render_data = render_data.lock().unwrap();
+            render_data.buffer.copy_into(&mut buffers[next_idx]);
+            cursor_pos = render_data.cursor_pos;
+        }
+
+        // In the beginning of a connection, the buffer isn't ready yet
+        if buffers[next_idx].width != 0 && buffers[next_idx].height != 0 {
+            let to_send =
+                buffers[next_idx].get_updates_as_ansi_codes(&buffers[1 - next_idx], cursor_pos);
+            writer.write_all(to_send.as_bytes()).await?;
+        }
+
+        next_idx = 1 - next_idx;
+        let change_notify = render_data.lock().unwrap().changed.clone();
+        change_notify.notified().await;
+    }
+}
+
+fn log_ip_if_connects_a_lot(
+    logger: &client::ClientLogger,
+    ip: IpAddr,
+    recent_ips: Arc<Mutex<VecDeque<(Instant, IpAddr)>>>,
+) {
+    let n;
+    {
+        let mut recent_ips = recent_ips.lock().unwrap();
+        recent_ips.push_back((Instant::now(), ip));
+        while recent_ips.len() != 0 && recent_ips[0].0.elapsed().as_secs_f32() > 60.0 {
+            recent_ips.pop_front();
+        }
+        n = recent_ips
+            .iter()
+            .filter(|(_, recent_ip)| *recent_ip == ip)
+            .count();
+    }
+
+    if n >= 5 {
+        logger.log(&format!(
+            "This is the {}th connection from IP address {} within the last minute",
+            n, ip
+        ));
+    }
+}
+
+pub async fn handle_connection(
+    socket: TcpStream,
+    ip: IpAddr,
+    lobbies: lobby::Lobbies,
+    used_names: Arc<Mutex<HashSet<String>>>,
+    recent_ips: Arc<Mutex<VecDeque<(Instant, IpAddr)>>>,
+) {
+    // TODO: max concurrent connections from same ip?
+    let (reader, mut writer) = socket.into_split();
+
+    let client = client::Client::new(reader);
+    let logger = client.logger();
+    logger.log("New connection");
+    log_ip_if_connects_a_lot(&logger, ip, recent_ips);
+    let render_data = client.render_data.clone();
+
+    let result: Result<(), io::Error> = tokio::select! {
+        res = handle_receiving(client, lobbies, used_names) => res,
+        res = handle_sending(&mut writer, render_data) => res,
+    };
+
+    // Try to leave the terminal in a sane state
+    let cleanup_ansi_codes = ansi::SHOW_CURSOR.to_owned()
+        + &ansi::move_cursor_horizontally(0)
+        + ansi::CLEAR_FROM_CURSOR_TO_END_OF_SCREEN;
+    _ = timeout(
+        Duration::from_millis(500),
+        writer.write_all(cleanup_ansi_codes.as_bytes()),
+    )
+    .await;
+
+    logger.log(&format!("Disconnected: {}", result.unwrap_err()));
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    crate::high_scores::add_high_score(crate::high_scores::GameResult {
-        mode: Mode::Ring,
-        score: 123,
-        duration: Duration::from_millis(678901),
-        players: vec!["John".to_string(), "Bob".to_string(), "Mary".to_string()],
-    })
-    .await?;
-    Ok(())
+async fn main() {
+    let listener = TcpListener::bind("0.0.0.0:12345").await.unwrap();
+
+    let used_names = Arc::new(Mutex::new(HashSet::new()));
+    let lobbies: lobby::Lobbies = Arc::new(Mutex::new(WeakValueHashMap::new()));
+    let recent_ips = Arc::new(Mutex::new(VecDeque::new()));
+
+    loop {
+        let (socket, sockaddr) = listener.accept().await.unwrap();
+        let lobbies = lobbies.clone();
+        tokio::spawn(handle_connection(
+            socket,
+            sockaddr.ip(),
+            lobbies.clone(),
+            used_names.clone(),
+            recent_ips.clone(),
+        ));
+    }
 }
