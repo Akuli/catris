81073cae1264de467e1e3e5da6c3a1ab5e4e3233
Akuli <akuviljanen17@gmail.com>
2022-06-16 18:59:29 +0300

    websockets: hello world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#[macro_use(lazy_static)]
+/*#[macro_use(lazy_static)]
 extern crate lazy_static;
 
 use crate::client::Client;
@@ -194,4 +194,47 @@ async fn main() {
             client_counter.clone(),
         ));
     }
+}
+*/
+
+
+
+use std::{env, io::Error};
+
+use futures_util::{future, StreamExt, TryStreamExt};
+use log::info;
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:54321".to_string());
+
+    // Create the event loop and TCP listener we'll accept connections on.
+    let try_socket = TcpListener::bind(&addr).await;
+    let listener = try_socket.expect("Failed to bind");
+    info!("Listening on: {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(accept_connection(stream));
+    }
+
+    Ok(())
+}
+
+async fn accept_connection(stream: TcpStream) {
+    let addr = stream.peer_addr().expect("connected streams should have a peer address");
+    info!("Peer address: {}", addr);
+
+    let ws_stream = tokio_tungstenite::accept_async(stream)
+        .await
+        .expect("Error during the websocket handshake occurred");
+
+    info!("New WebSocket connection: {}", addr);
+
+    let (write, read) = ws_stream.split();
+    // We should not forward messages other than text or binary.
+    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
+        .forward(write)
+        .await
+        .expect("Failed to forward messages")
 }
