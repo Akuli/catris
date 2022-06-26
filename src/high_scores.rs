b4438573a35ce6e9602d41f4c99474ebf23763e3
Akuli <akuviljanen17@gmail.com>
2022-06-26 21:26:46 +0300

    comment

--- src/high_scores.rs
+++ src/high_scores.rs
@@ -8,6 +8,7 @@ use std::io::SeekFrom;
 use std::io::Write;
 use std::time::Duration;
 
+// https://users.rust-lang.org/t/convert-box-dyn-error-to-box-dyn-error-send/48856/8
 type AnyErrorThreadSafe = Box<dyn std::error::Error + Send + Sync>;
 
 #[derive(Debug, Clone, PartialEq)]
