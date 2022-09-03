e5a3445783ffcd6c13c78f3d116840af63941b52
Akuli <akuviljanen17@gmail.com>
2022-09-03 19:11:05 +0300

    new erasing logic, improved scrolling

--- src/game_logic/game.rs
+++ src/game_logic/game.rs
@@ -142,11 +142,15 @@ In adventure mode, the map always extends this much above and below the top of t
 This way:
     - A block can bump into landed square before the block becomes visible.
     - The landing places prediction will work even for blocks that aren't visible yet.
+    - When a row is removed and rows move down in the view, we don't need to generate
+      rows on the top of the view because we still remember enough old rows.
+      For this to work we need a lot of extra rows on top.
 
 With multiple players, it's also possible to move blocks more than this amount below
 In that case, the map will be generated enough for the lowest square of that block.
 */
-const ADVENTURE_EXTRA_ROWS: usize = 10;
+const ADVENTURE_EXTRA_ROWS_TOP: usize = 50;
+const ADVENTURE_EXTRA_ROWS_BOTTOM: usize = 5;
 
 struct AdventureModeData {
     scroll_count: usize,
@@ -250,7 +254,7 @@ impl Game {
         match self.mode {
             Mode::Traditional | Mode::Bottle => (0, 0),
             Mode::Ring => (RING_OUTER_RADIUS as i16, RING_OUTER_RADIUS as i16),
-            Mode::Adventure => (0, ADVENTURE_EXTRA_ROWS as i16),
+            Mode::Adventure => (0, ADVENTURE_EXTRA_ROWS_TOP as i16),
         }
     }
 
@@ -335,7 +339,7 @@ impl Game {
         if self.mode == Mode::Adventure {
             let i = landed_rows_index + self.adventure_data.as_ref().unwrap().scroll_count;
             // Start game without rows, after that add two consecutive rows
-            if i > ADVENTURE_EXTRA_ROWS + self.get_height() && (i % 15 == 0 || i % 15 == 1) {
+            if i > ADVENTURE_EXTRA_ROWS_TOP + self.get_height() && (i % 15 == 0 || i % 15 == 1) {
                 return Some(SquareContent::with_color(Color::WHITE_BACKGROUND));
             }
         }
@@ -347,11 +351,19 @@ impl Game {
             return;
         }
 
-        // TODO: account for blocks below view
-        let biggest_y_used = ADVENTURE_EXTRA_ROWS + self.get_height();
-        let min_count = biggest_y_used + ADVENTURE_EXTRA_ROWS;
+        let bottom = ADVENTURE_EXTRA_ROWS_TOP + self.get_height();
+        let biggest_y_used = self
+            .players
+            .iter()
+            .map(|p| p.borrow().block_or_timer.get_coords())
+            .flatten()
+            .map(|(_, y)| y)
+            .chain([bottom as i32].into_iter())
+            .max()
+            .unwrap();
+        let min_count = biggest_y_used + (ADVENTURE_EXTRA_ROWS_BOTTOM as i32);
 
-        while self.landed_rows.len() < min_count {
+        while (self.landed_rows.len() as i32) < min_count {
             let fill = self.get_fill_square(self.landed_rows.len());
             let mut row = vec![fill; self.get_width()];
 
@@ -377,8 +389,13 @@ impl Game {
                 (true, true, false) => Some("'."),  // wall ends
                 (false, true, false) => Some("'~"), // single-square wall
             };
-            row[0] = left_text.map(|s| SquareContent::with_text(s));
-            *row.last_mut().unwrap() = right_text.map(|s| SquareContent::with_text(s));
+
+            if let Some(s) = left_text {
+                row[0] = Some(SquareContent::with_text(s));
+            }
+            if let Some(s) = right_text {
+                *row.last_mut().unwrap() = Some(SquareContent::with_text(s));
+            }
 
             // Put a hole in a random location, avoiding the walls if we added any
             let mut range_start = 0usize;
@@ -560,7 +577,7 @@ impl Game {
                 for (row_index, row) in self.landed_rows.iter().enumerate() {
                     if !row.iter().any(|cell| cell.is_none()) {
                         full_count_everyone += 1;
-                        let y = (row_index as i16) - (ADVENTURE_EXTRA_ROWS as i16);
+                        let y = (row_index as i16) - (ADVENTURE_EXTRA_ROWS_TOP as i16);
                         for (x, _) in row.iter().enumerate() {
                             full_points.push((x as i16, y as i16));
                         }
@@ -636,13 +653,11 @@ impl Game {
                 }
             }
             Mode::Adventure => {
-                // When a row is removed, let the rows below it slide up.
-                // This kind of deletion messes up indexes of the moved rows below.
-                // To avoid problems with deleting multiple rows, we must go from bottom to top.
                 for i in (0..self.landed_rows.len()).rev() {
-                    let y = (i as i16) - (ADVENTURE_EXTRA_ROWS as i16);
+                    let y = (i as i16) - (ADVENTURE_EXTRA_ROWS_TOP as i16);
                     if full.contains(&(0, y)) {
                         self.landed_rows.remove(i);
+                        self.landed_rows.insert(0, vec![None; self.get_width()]);
                     }
                 }
                 self.generate_rows();
@@ -778,10 +793,10 @@ impl Game {
             Mode::Adventure => {
                 // Need to place some restrictions on y so that landing prediction code doesn't error.
                 // Otherwise it tries to access landed rows beyond what was generated so far.
-                // We generate ahead by at least ADVENTURE_EXTRA_ROWS, so enough to not be visible to users
+                // We generate ahead by at least ADVENTURE_EXTRA_ROWS_BOTTOM, so enough to not be visible to users
                 let w = self.get_width() as i16;
-                let h = (self.landed_rows.len() - ADVENTURE_EXTRA_ROWS) as i16;
-                (0..w).contains(&x) && (0..h).contains(&y)
+                let h = (self.landed_rows.len() - ADVENTURE_EXTRA_ROWS_TOP) as i16;
+                (0..w).contains(&x) && (..h).contains(&y)
             }
         }
     }
@@ -1026,8 +1041,7 @@ impl Game {
             }
         }
 
-        // TODO: this scrolls a bit too fast, make separate task
-        if self.mode == Mode::Adventure && fast {
+        if self.mode == Mode::Adventure {
             // Keep highest moving block visible
             let smallest_center_y = self
                 .players
@@ -1036,10 +1050,10 @@ impl Game {
                     BlockOrTimer::Block(b) => Some(b.center.1),
                     _ => None,
                 })
-                .min().unwrap_or(-1);
+                .min()
+                .unwrap_or(-1);
 
-            if smallest_center_y > self.get_height() as i32 / 3
-            {
+            if smallest_center_y > 15 {
                 self.scroll_down();
                 need_render = true;
             }
@@ -1422,7 +1436,7 @@ impl Game {
                 let right = w * (player_idx + 1);
 
                 // Avoid deleting below the visibile part by only deleting the first n rows
-                let n = ADVENTURE_EXTRA_ROWS + self.get_height();
+                let n = ADVENTURE_EXTRA_ROWS_TOP + self.get_height();
                 for row in self.landed_rows[0..n].iter_mut() {
                     for square_ref in row[left..right].iter_mut() {
                         *square_ref = None;
