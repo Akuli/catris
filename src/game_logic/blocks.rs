dcedff7fc8e4ad9cfafd97ca868b646c44ad4a45
Akuli <akuviljanen17@gmail.com>
2022-11-13 21:02:23 +0200

    cargo fmt

--- src/game_logic/blocks.rs
+++ src/game_logic/blocks.rs
@@ -558,7 +558,7 @@ impl FallingBlock {
     }
 
     pub fn set_player_coords(&mut self, coords: &[PlayerPoint], new_center: PlayerPoint) {
-        assert!(coords.len() != 0);  // instead of making empty block, just give a new block to the player
+        assert!(coords.len() != 0); // instead of making empty block, just give a new block to the player
         self.center = new_center;
         let (cx, cy) = new_center;
         self.relative_coords = coords
