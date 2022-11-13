61d08b7cf3d306319c555224b62dc0a2406ce3c9
Akuli <akuviljanen17@gmail.com>
2022-11-14 00:06:22 +0200

    Fix

--- src/escapes.rs
+++ src/escapes.rs
@@ -2,7 +2,7 @@ use std::fmt::Write;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum TerminalType {
-    ANSI, // Modern terminals https://en.wikipedia.org/wiki/ANSI_escape_code
+    Ansi, // Modern terminals https://en.wikipedia.org/wiki/ANSI_escape_code
     VT52, // Older devices https://en.wikipedia.org/wiki/VT52
 }
 
