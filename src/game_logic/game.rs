421def7da1473095c069148bbbfd78e82fe80701
Akuli <akuviljanen17@gmail.com>
2022-11-19 00:10:37 +0200

    Actually, lets get rid of the inner radius

--- src/game_logic/game.rs
+++ src/game_logic/game.rs
@@ -154,7 +154,6 @@ pub const RING_MAP: &[&str] = &[
     "               'o------------------------------------------o'               ",
 ];
 pub const RING_OUTER_RADIUS: i16 = 18;
-const RING_INNER_RADIUS: i16 = 3;
 
 pub fn wrap_around(mode: Mode, y: &mut i32) {
     if mode == Mode::Ring && *y > 0 {
@@ -515,7 +514,7 @@ impl Game {
                 }
             }
             Mode::Ring => {
-                for r in (RING_INNER_RADIUS + 1)..=RING_OUTER_RADIUS {
+                for r in 1..=RING_OUTER_RADIUS {
                     if square(r)
                         .iter()
                         .all(|p| self.get_landed_square(*p).is_some())
